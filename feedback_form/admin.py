d7ae89bd5a107faf79f8dde6fe06505a136d2387
cericoda <cericoda@altfa.com>
2014-07-21 09:43:11 +0100

    get_user_email

--- feedback_form/admin.py
+++ feedback_form/admin.py
@@ -9,7 +9,7 @@ from .models import Feedback
 class FeedbackAdmin(admin.ModelAdmin):
     """Admin class for the ``Feedback`` model."""
     list_display = [
-        'creation_date', 'user_email', 'current_url', 'message_excerpt', ]
+        'creation_date', 'get_user_email', 'current_url', 'message_excerpt', ]
     list_filter = ['creation_date', 'current_url', ]
     date_hierarchy = 'creation_date'
     search_fields = ['user__email', 'email', 'current_url', 'message', ]
@@ -18,11 +18,7 @@ class FeedbackAdmin(admin.ModelAdmin):
         return truncatewords(obj.message, 10)
     message_excerpt.short_description = _('Message excerpt')
 
-    def user_email(self, obj):
-        if obj.user:
-            return obj.user.email
-        return obj.email
-    user_email.short_description = _('Email')
+    get_user_email.short_description = _('Email')
 
 
 admin.site.register(Feedback, FeedbackAdmin)
